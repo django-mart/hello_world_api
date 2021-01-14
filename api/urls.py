c9318fc949f07d14060e78222e5d206ca1a9c4f8
Yash Jain <yash0307jain@gmail.com>
2021-01-14 12:33:51 +0530

    add some renderer and name a view

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import HelloView
 
 urlpatterns = [
-    url(r'^$', HelloView.as_view()),
+    url(r'^$', HelloView.as_view(), name='hello'),
 ]
--- hello_world/settings.py
+++ hello_world/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
